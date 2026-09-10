6faef4dac54867a04bf91ffdeba1f8a1ca867f05
borbagustavo <gustavo.borba.27@gmail.com>
2026-09-10 20:45:47 -0300

    feat: usando useEffect, e construindo uma tabela

---
+++ my-app/src/data/listaProdutos.ts
@@ -0,0 +1,7 @@
+import type { TipoProduto } from "../types/types";
+
+export const listaProdutos : TipoProduto[] = [
+  { id: 1, nome: "Produto 1", preco: 10.0, descricao: "Descrição do Produto 1", avatar: "avatar1.png" },
+  { id: 2, nome: "Produto 2", preco: 20.0, descricao: "Descrição do Produto 2", avatar: "avatar2.png" },
+  { id: 3, nome: "Produto 3", preco: 30.0, descricao: "Descrição do Produto 3", avatar: "avatar3.png" },
+];
--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,7 +1,54 @@
 
+import { useEffect, useState } from "react";
+import type { TipoProduto } from "../../types/types";
+import { listaProdutos } from "../../data/listaProdutos";
+import { Link } from "react-router";
+Link
+
 
 export default function Produtos() {
+  document.title = "Produtos"
+
+ const [produtos, setProdutos] = useState<TipoProduto[]>([])
+
+ useEffect(() => {
+  setProdutos(listaProdutos);
+  }, [])
+
   return (
-    <div>Produtos</div>
+    <main>
+      <h2>Produtos</h2>
+
+    <table border={1} cellPadding={10} style={{ width: '100%', borderCollapse: 'collapse' }}>
+      <thead>
+        <tr style={{ backgroundColor: '#be7310', color: '#050505' }}>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Preço</th>
+          <th>Descrição</th>
+          <th>Avatar</th>
+          <th>Editar</th>
+        </tr>
+      </thead>
+      <tbody>
+        {produtos.map((p) => (
+          <tr key={p.id}>
+            <td>{p.id}</td>
+            <td>{p.nome}</td>
+            <td>{p.preco.toFixed(2)}</td>
+            <td>{p.descricao}</td>
+            <td><img src={p.avatar} alt={p.nome} width={60} height={60} style={{ objectFit: 'cover' }} /></td>
+            <td><Link to={`/produtos/${p.id}`}>Editar</Link></td>
+          </tr>
+        ))}
+      </tbody>
+      <tfoot>
+    </tfoot>
+    <tr>
+      <td>Quantidade de produtos - {produtos.length}</td>
+    </tr>
+    </table>
+  
+    </main>
   )
 }

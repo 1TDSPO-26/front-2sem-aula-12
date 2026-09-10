ccc8f796268da24482363b7537ca6381ec08d955
MuriloSCruzz <cruzmurilo23@gmail.com>
2026-09-10 20:39:43 -0300

    feat: trabalhando com useEffect e map() para resgatar produtos de uma base de dados

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,11 +1,54 @@
+import { useEffect, useState } from "react"
+import type { TipoProduto } from "../../types/types";
+import { listaProdutos } from "../../data/listaProdutos";
+import { Link } from "react-router";
 
 export default function Produtos(){
 
     document.title = "Produtos"
 
+    const[produtos, setProdutos] = useState<TipoProduto[]>([]);
+    
+    useEffect( ()=>{
+
+        setProdutos(listaProdutos);
+
+    },[] );
+
     return(
         <main>
             <h2>Produtos</h2>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                        <th>Descrição</th>
+                        <th>Avatar</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {produtos.map( ( p )=> (
+                        <tr key={p.id}>
+                            <td>{p.id}</td>
+                            <td>{p.nome}</td>
+                            <td>{p.preco}</td>
+                            <td>{p.descricao}</td>
+                            <td><img src={p.avatar} alt={p.descricao} /></td>
+                            <td><Link to={`/editar-produtos/${p.id}`}>Editar</Link></td>
+                        </tr>
+                    ))}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td>Quantidade de produtos = {produtos.length}</td>
+                    </tr>
+                </tfoot>
+            </table>
+
         </main>
     )
 }

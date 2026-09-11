7a7943254d9bdebfb9a96f37035a38bab6381604
Matheus <rm573416@fiap.com.br>
2026-09-10 21:49:08 -0300

    feat: Lista de produtos mock e interface criada, e listagem de dados

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Cabecalho from "./components/Cabecalho";
+import Conteudo from "./components/Conteudo";
+import Rodape from "./components/Rodape";
+
+export default function App() {
+  return (
+    <div>
+      <Cabecalho/>
+      <Conteudo/>
+      <Rodape/>
+    </div>
+  )
+}
---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Cabecalho() {
+  return (
+    <header>
+        <h1>Meu Boiler Plate</h1>
+    </header>
+  )
+}
